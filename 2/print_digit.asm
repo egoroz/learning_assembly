8016ddf0f853bc9adaa5b758f26942d15e1a680e
egororo <egorsemirenko04@gmail.com>
2024-09-25 17:03:23 +0300

    some modifications

--- 2/print_digit.asm
+++ 2/print_digit.asm
@@ -1,6 +1,12 @@
 section .data
     digit db 0,10
 section .text
+%macro exit 0
+    mov rax, 60
+    mov rdi, 0
+    syscall
+%endmacro
+
 %macro printDigit 1
     mov rax, %1
     call _printDigit
@@ -9,10 +15,10 @@ section .text
     global _start
 _start:
     printDigit 7
+    printDigit 9
+    printDigit 0
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    exit
 
 _printDigit:
     add rax, 48
