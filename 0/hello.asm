41ddb71ff754291c3c436dd2c50f2e2d17221038
egororo <egorsemirenko04@gmail.com>
2024-09-25 17:00:21 +0300

    finish 0

--- 0/hello.asm
+++ 0/hello.asm
@@ -3,12 +3,17 @@ section .data
 section .text
     global _start
 _start:
+
+    call _hello
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_hello:
     mov rax, 1
     mov rdi, 1
     mov rsi, text
     mov rdx, 14
     syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    ret
